075fa5e31179566196324b72f4edcae92b5b044e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-08 14:22:21 +0000

    Deploy rust-lang/chalk to github.com/rust-lang/chalk.git:gh-pages

---
+++ doc/chalk_solve/wf/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"constant":[["_DERIVE_failure_Fail_FOR_WfError",""],["_DERIVE_failure_core_fmt_Display_FOR_WfError",""]],"enum":[["WfError",""]],"struct":[["WfSolver",""]],"trait":[["FoldInputTypes","A trait for retrieving all types appearing in some Chalk construction."]]});
